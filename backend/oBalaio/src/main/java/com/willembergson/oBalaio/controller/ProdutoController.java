fd489f390c438ee1899cf72f0211def11db85c00
wilembergson <wilembergson@yahoo.com>
2021-05-17 22:00:11 -0300

    Produto CRUD

--- backend/oBalaio/src/main/java/com/willembergson/oBalaio/controller/ProdutoController.java
+++ backend/oBalaio/src/main/java/com/willembergson/oBalaio/controller/ProdutoController.java
@@ -1,10 +1,13 @@
 package com.willembergson.oBalaio.controller;
 
+import com.willembergson.oBalaio.dto.ProdutoDTO;
 import com.willembergson.oBalaio.entity.Produto;
 import com.willembergson.oBalaio.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
@@ -16,10 +19,27 @@ public class ProdutoController {
         this.produtoService = produtoService;
     }
 
+    @GetMapping
+    public List<ProdutoDTO> findAll(){
+        return produtoService.findAll();
+    }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Produto findById(@PathVariable Long id) throws Exception{
         return produtoService.findById(id);
     }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws Exception {
+        produtoService.delete(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduto(@RequestBody Produto produto){
+        produtoService.create(produto);
+    }
+
 }
--- backend/oBalaio/src/main/java/com/willembergson/oBalaio/services/ProdutoService.java
+++ backend/oBalaio/src/main/java/com/willembergson/oBalaio/services/ProdutoService.java
@@ -1,9 +1,13 @@
 package com.willembergson.oBalaio.services;
 
+import com.willembergson.oBalaio.dto.ProdutoDTO;
 import com.willembergson.oBalaio.entity.Produto;
 import com.willembergson.oBalaio.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProdutoService {
@@ -15,8 +19,22 @@ public class ProdutoService {
         this.produtoRepository = produtoRepository;
     }
 
+    public List<ProdutoDTO> findAll(){
+        List<Produto> list = produtoRepository.findAll();
+        return list.stream().map(x -> new ProdutoDTO(x)).collect(Collectors.toList());
+    }
+
     public Produto findById(Long id) throws Exception{
         Produto produto = produtoRepository.findById(id).orElseThrow(() -> new Exception());
         return produto;
+    }
+
+    public void delete(Long id) throws  Exception{
+        produtoRepository.findById(id).orElseThrow(() -> new Exception());
+        produtoRepository.deleteById(id);
+    }
+
+    public void create(Produto produto){
+        produtoRepository.save(produto);
     }
 }
