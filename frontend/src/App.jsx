a3d085d8eab1c245abd0f8f76001288626a86a05
wilembergson <wilembergson@yahoo.com>
2021-05-19 17:42:21 -0300

    Frontend continues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import ProdutoComponent from './components/ProdutoComponent';
+
 
 function App() {
   return (
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+
+function NavBar() {
+    return (
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+            <div className="container">
+                <nav className="navbar navbar-dark bg-primary">
+                    <h3>Teste</h3>
+                </nav>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
