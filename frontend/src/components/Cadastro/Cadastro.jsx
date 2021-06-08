1e8ed1dfa9e86c56de696d2b886dd79daa4c724b
wilembergson <wilembergson@yahoo.com>
2021-06-07 22:33:07 -0300

    Deletar produto frontend

--- frontend/src/components/Cadastro/Cadastro.jsx
+++ frontend/src/components/Cadastro/Cadastro.jsx
@@ -3,10 +3,9 @@ import './Cadastro.css'
 import axios from 'axios'
 import { BsFillTrashFill } from 'react-icons/bs'
 import { GrEdit } from 'react-icons/gr'
-import { Redirect } from 'react-router'
+import { AiFillSave } from 'react-icons/ai'
 
 const API_URL = 'http://localhost:8080/produtos'
-const API_URL_LISTALL = 'http://localhost:8080/produtos/listAll'
 
 const produtoInicial = {
     name: '',
@@ -18,7 +17,7 @@ function Cadastro() {
     const [prod, setProd] = useState([])
     const [produto, setProduto] = useState(produtoInicial)
 
-    axios.get(API_URL_LISTALL).then((response) => setProd(response.data))
+    axios.get(`${API_URL}/listAll`).then((response) => setProd(response.data))
 
     function onChange(evento) {
         const { className, value } = evento.target
@@ -30,6 +29,11 @@ function Cadastro() {
         evento.preventDefault()
 
         axios.post(API_URL, produto)
+    }
+
+    function deletar(p) {
+        axios.delete(`${API_URL}/${p.id}`)
+        alert(`O produto ${p.name} foi excluído com sucesso.`)
     }
 
     return (
@@ -46,41 +50,41 @@ function Cadastro() {
                         </div>
                         <div className="cadastro-campo">
                             <label htmlFor="price">Preço</label>
-                            <input id="price" className="price" type="number" onChange={onChange} step="0.01"/>
+                            <input id="price" className="price" type="number" onChange={onChange} step="0.01" />
                         </div>
                         <div>
                             {/*Adicionar efeito hover no botão*/}
-                            <button type="submit" className="bt-salvar">Salvar</button>
+                            <button type="submit" className="bt-salvar"><AiFillSave /> Salvar</button>
                         </div>
                     </form>
                     <hr className="sss" />
                     <div className="cadastro-campo">
-                    <h3 className="tt">Produtos cadastrados</h3>
-                    <table className="table table-striped">
-                        <thead>
-                            <tr>
-                                <td>ID</td>
-                                <td>NOME</td>
-                                <td>PREÇO</td>
-                                <td></td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                prod.map(p =>
-                                    <tr key={p.id}>
-                                        <td>{p.id}</td>
-                                        <td>{p.name}</td>
-                                        <td>R${p.price.toFixed(2)}</td>
-                                        <td><button type="button" className="btn btn-danger"><BsFillTrashFill/></button>
-                                            <button type="button" className="btn btn-warning"><GrEdit/></button>
-                                        </td>
-                                    </tr>
+                        <h3 className="tt">Produtos cadastrados</h3>
+                        <table className="table table-striped">
+                            <thead>
+                                <tr>
+                                    <td>ID</td>
+                                    <td>NOME</td>
+                                    <td>PREÇO</td>
+                                    <td></td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    prod.map(p =>
+                                        <tr key={p.id}>
+                                            <td>{p.id}</td>
+                                            <td>{p.name}</td>
+                                            <td>R${p.price.toFixed(2)}</td>
+                                            <td><button type="button" onClick={() => deletar(p)} className="btn btn-danger"><BsFillTrashFill /></button>
+                                                <button type="button" className="btn btn-warning"><GrEdit /></button>
+                                            </td>
+                                        </tr>
 
-                                )
-                            }
-                        </tbody>
-                    </table>
+                                    )
+                                }
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
