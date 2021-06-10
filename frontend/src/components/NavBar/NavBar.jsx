66de79b9948bfcf4d24ea13af4d5f08132057ef8
wilembergson <wilembergson@yahoo.com>
2021-06-10 16:43:09 -0300

    Logo

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -4,12 +4,13 @@ import { MdShoppingCart } from 'react-icons/md'
 import { FaListAlt } from 'react-icons/fa'
 import { FaTasks } from 'react-icons/fa'
 import { ImHome3 } from 'react-icons/im'
+import Logo from '../../img/logo.png'
 
 function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">oBalaio</a>
+                <a className="navbar-brand" href="/"><img src={Logo}/></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
